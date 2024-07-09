837c591cd484b4df590cd8b61578a2f394a597fd
charistalenavi <muhammad.charis@talenavi.com>
2024-07-09 15:52:40 +0700

    style: fixing logo margin

--- src/app/[locale]/register/[type]/page.jsx
+++ src/app/[locale]/register/[type]/page.jsx
@@ -19,7 +19,7 @@ const RegisterEmailPhonePage = async ({ params }) => {
     <div className="flex min-h-dvh flex-col justify-between">
       <div className="flex flex-1 flex-col items-center  justify-center ">
         <div className="mt-6 flex w-full flex-1 flex-col items-center px-28">
-          <Logo />
+          <Logo className="mt-41 h-40.8 w-192" />
           <Heading
             className="mt-28.5 text-center text-2122 font-medium"
             key="createAccount"
