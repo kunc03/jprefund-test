4be70e1fffef1b164b5f1a28992a5394e4509c52
charistalenavi <muhammad.charis@talenavi.com>
2024-07-12 20:57:02 +0700

    feat: add page faq

---
+++ src/app/[locale]/faq/_components/accordion.jsx
@@ -0,0 +1,60 @@
+'use client';
+
+import {
+  Accordion as UIAccordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components';
+
+import { useState } from 'react';
+import Image from 'next/image';
+
+const Accordion = ({ label, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <UIAccordion
+      type="single"
+      collapsible
+      className="w-full"
+      onValueChange={(value) => setIsOpen(value !== '')}
+    >
+      <AccordionItem
+        value={`${label}`}
+        className="!border-b border-b-gray-400 bg-white px-18 py-24"
+      >
+        <AccordionTrigger className="flex w-full cursor-default flex-row ">
+          <Image src="/icons/q.svg" width={24} height={24} alt="icon" />
+          <p className="flex-1 text-1521 font-medium text-gray">{label}</p>
+          {!isOpen && (
+            <Image
+              src="/icons/plus.svg"
+              width={16}
+              height={2}
+              alt="close"
+              className="cursor-pointer"
+            />
+          )}
+          {isOpen && (
+            <Image
+              src="/icons/minus.svg"
+              width={16}
+              height={16}
+              alt="plus"
+              className="cursor-pointer"
+            />
+          )}
+        </AccordionTrigger>
+        <AccordionContent className="! w-full py-20 md:max-w-md">
+          <p className="text-start text-1422 font-normal text-black">
+            {' '}
+            {content}
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+    </UIAccordion>
+  );
+};
+
+export { Accordion };
