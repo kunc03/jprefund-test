36a501600d713a4ac509ef4ce8c79c5a491ca2a4
Bagus Kuncoro <bagus.kuncoro@talenavi.com>
2024-07-16 06:18:53 +0700

    fix: feedback 150724

--- src/app/[locale]/register/[type]/otp/page.jsx
+++ src/app/[locale]/register/[type]/otp/page.jsx
@@ -15,7 +15,7 @@ const RegisterOtpPage = ({ params }) => {
   return (
     <div className="flex min-h-dvh flex-col justify-between">
       <div className="flex flex-1 flex-col items-center  justify-center ">
-        <div className="mt-6 flex w-full flex-1 flex-col items-center">
+        <div className="flex w-full flex-1 flex-col items-center">
           <div className="flex w-full flex-col items-center px-28">
             <Logo className="mt-41 h-40.8 w-192" />
             <OtpInformation type={type} />
