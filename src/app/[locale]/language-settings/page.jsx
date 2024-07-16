8aebf2d9660e71b319a9d16f00c5526750c35c7c
Bagus Kuncoro <bagus.kuncoro@talenavi.com>
2024-07-16 09:40:03 +0700

    feat: language settings

--- src/app/[locale]/language-settings/page.jsx
+++ src/app/[locale]/language-settings/page.jsx
@@ -1,28 +1,24 @@
 import { Header } from '@/components';
-import { cn } from '@/utils';
 import { getTranslations } from 'next-intl/server';
 import React from 'react';
+import { SelectLanguage } from './_components/select-language';
 
 export const generateMetadata = async ({ params }) => {
   const { locale } = params;
-  const t = await getTranslations({ locale, namespace: 'privacyPolicy' });
+  const t = await getTranslations({ locale, namespace: 'languageSettings' });
 
   return { title: t('metaTitle'), description: t('metaDescription') };
 };
 
 const PrivacyPolicy = async ({ params }) => {
   const { locale } = params;
-  const t = await getTranslations({ locale, namespace: 'privacyPolicy' });
+  const t = await getTranslations({ locale, namespace: 'languageSettings' });
 
   return (
-    <div className="flex min-h-dvh flex-col items-center">
+    <div className="flex min-h-dvh flex-col items-center bg-white">
       <Header hasBack title={t('title')} />
 
-      <div className={cn('text-1425 flex flex-col gap-5 p-9 text-justify')}>
-        <p>{t('content1')}</p>
-        <p>{t('content2')}</p>
-        <p>{t('content3')}</p>
-      </div>
+      <SelectLanguage />
     </div>
   );
 };
