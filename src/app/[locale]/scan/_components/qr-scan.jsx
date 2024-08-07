88790051823aa58c61c2ccb64627e1f10e145877
Bagus Kuncoro <bagus.kuncoro@talenavi.com>
2024-08-07 14:01:33 +0700

    fix: scan barcode

--- src/app/[locale]/scan/_components/qr-scan.jsx
+++ src/app/[locale]/scan/_components/qr-scan.jsx
@@ -5,81 +5,53 @@
 'use client';
 
 // import QrScanner from 'qr-scanner';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useQrScan } from '@/hooks';
 import { cn } from '@/utils';
-import { BarcodeReader } from 'dynamsoft-javascript-barcode';
+import { useZxing } from 'react-zxing';
 
 const QrScan = () => {
-  const scanner = useRef(null);
-  const videoEl = useRef(null);
-  const qrBoxEl = useRef(null);
-  const [qrOn, setQrOn] = useState(true);
+  const [result, setResult] = useState('');
   const { onSelected } = useQrScan();
+  const [showVideoFeed, setShowVideoFeed] = useState(true);
   const router = useRouter();
 
-  const onScanSuccess = (result) => {
-    if (result && result.length > 0) {
-      onSelected(result[0].barcodeText);
+  const { ref } = useZxing({
+    onDecodeResult(decodeResult) {
+      setResult(decodeResult.getText());
+      setShowVideoFeed(false);
+    },
+  });
+
+  useEffect(() => {
+    if (result) {
+      onSelected(result);
       router.push('/home');
     }
-  };
+  }, [result, onSelected, router]);
 
-  useEffect(() => {
-    const initializeScanner = async () => {
-      if (videoEl?.current && !scanner.current) {
-        try {
-          // Initialize Dynamsoft Barcode Reader
-          scanner.current = await BarcodeReader.createInstance();
-
-          // Start video scanning
-          await scanner.current.setVideoElement(videoEl.current);
-          scanner.current.onFrameRead = onScanSuccess;
-          await scanner.current.open();
-          setQrOn(true);
-        } catch (err) {
-          console.error('Scanner start error:', err);
-          setQrOn(false);
-        }
-      }
-    };
-
-    initializeScanner();
-
-    return () => {
-      if (scanner.current) {
-        scanner.current.destroyContext();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!qrOn) {
-      alert(
-        'Camera is blocked or not accessible. Please allow camera in your browser permissions and Reload.',
-      );
-    }
-  }, [qrOn]);
+  console.log(showVideoFeed);
 
   return (
     <div
       className={cn('camera-container')}
       style={{ width: '100%', height: '100%' }}
     >
-      <div
+      {/* <div
         className={cn(
           'flex w-full h-screen items-center justify-center relative',
         )}
       >
         <div
+          id="qr-reader"
           ref={qrBoxEl}
           className={cn('scanner absolute top-[100px]')}
           style={{ width: '360px', height: '360px' }}
         />
-      </div>
+      </div> */}
 
-      <video ref={videoEl} autoPlay playsInline className={cn('camera')} />
+      <video ref={ref} autoPlay playsInline className={cn('camera')} />
     </div>
   );
 };
