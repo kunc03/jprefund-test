78cd86d924ea9e71f4316c255a8461ef084b03d1
Bagus Kuncoro <bagus.kuncoro@talenavi.com>
2024-08-14 10:19:43 +0700

    fix: background

--- src/app/[locale]/language-settings/_components/select-language.jsx
+++ src/app/[locale]/language-settings/_components/select-language.jsx
@@ -8,7 +8,7 @@ import { useState } from 'react';
 const SelectLanguage = () => {
   const t = useTranslations('languageSettings');
   const [selectedFinancialInstituteName, setSelectedFinancialInstituteName] =
-    useState(null);
+    useState('english');
 
   const languages = {
     english: 'english',
@@ -29,8 +29,7 @@ const SelectLanguage = () => {
             <div className="flex h-58 cursor-pointer flex-row items-center justify-between border-b border-b-gray-500 py-22.5 pl-8">
               <p className="text-1613 font-medium text-black">{t(language)}</p>
 
-              {(selectedFinancialInstituteName === language ||
-                language === 'english') && (
+              {selectedFinancialInstituteName === language && (
                 <Image
                   alt="checklist"
                   height={18}
