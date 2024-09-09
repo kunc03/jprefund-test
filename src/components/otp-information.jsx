2a9dfc6aa6f681626bc863ec9d998d85986b0551
Bagus Kuncoro <bagus.kuncoro@talenavi.com>
2024-09-09 17:05:19 +0700

    fix: translation

--- src/components/otp-information.jsx
+++ src/components/otp-information.jsx
@@ -3,30 +3,38 @@
 import { cn } from '@/utils';
 import { Heading } from './heading';
 import { useTranslations } from 'next-intl';
+import { usePathname } from 'next/navigation';
 
 const OtpInformation = ({ type }) => {
   const t = useTranslations('register');
+  const pathname = usePathname();
+
+  const accountInform = pathname.includes('account-information');
+
+  const boldContent = (
+    <span className="font-bold" key={accountInform ? 'authCode' : 'verifyCode'}>
+      {t(accountInform ? 'authCode' : 'verifyCode')}
+    </span>
+  );
+
+  const emailInform = accountInform
+    ? 'enterAuthCodeEmailAccountInform'
+    : 'enterAuthCodeEmail';
 
   return (
     <Heading
       className={cn('mt-28.5 mb-17 text-center text-1522 font-normal')}
-      key="verifyCodeSend"
+      key="otpInformation"
     >
       {type === 'email' &&
-        t.rich('enterAuthCodeEmail', {
-          authCode: (
-            <span className="font-bold" key="authCodeEmail">
-              {t('authCode')}
-            </span>
-          ),
+        t.rich(emailInform, {
+          authCode: boldContent,
+          verifyCode: boldContent,
         })}
       {type === 'phone' &&
         t.rich('enterAuthCodePhone', {
-          authCode: (
-            <span className="font-bold" key="authCodePhone">
-              {t('authCode')}
-            </span>
-          ),
+          authCode: boldContent,
+          verifyCode: boldContent,
         })}
     </Heading>
   );
